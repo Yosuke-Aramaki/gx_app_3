1edcc1e3fb78d3493bb5d618a461d28823f56847
Yosuke-Aramaki <gnx.vw903@gmail.com>
2020-07-13 09:23:49 +0900

    create erb

--- extension/script.js
+++ extension/script.js
@@ -7,10 +7,12 @@ chrome.tabs.getSelected( tab => {
 });
 
 window.addEventListener('load',()=>{  
+  
   chrome.tabs.getSelected( tab => { 
     siteData.title = tab.title;
     siteData.url = tab.url;    
   });
+
   document.getElementById('og_title_section').textContent = siteData.title;
   const url = siteData.url;
   fetch(url).then(res => res.text()).then(text => {
@@ -25,4 +27,32 @@ window.addEventListener('load',()=>{
       }
     })
   })
+
+  $("#loginButton").on("click", () => {
+  // document.getElementById('loginButton').addEventListener('click',()=>{  // クリックされたときにテキストボックスに出力
+    console.log(document.getElementById("email_address").value)
+    // var xhr = new XMLHttpRequest();
+
+    // var data = { session: { email: document.getElementById("email_address").value, password: document.getElementById("exampleInputPassword1").value } };
+    // var json_date = JSON.stringify(data);
+    // xhr.open("post", "http://localhost:3000/api/v1/login", true);
+    // // サーバに対して解析方法を指定する
+    // xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+    // // データをリクエスト ボディに含めて送信する
+    // xhr.send(json_date);
+    // xhr.addEventListener("load", function(){ 
+    //   console.log(this.response); 
+    //   document.getElementById('sample').textContent = this.response;
+    // }, false);
+
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:3000/api/v1/login",
+      data: { name: {email: document.getElementById("email_address").value, password: document.getElementById("exampleInputPassword1").value } }
+    })
+    .done(function(result) {
+      console.log(result);
+      document.getElementById('sample').textContent = this.response;
+    })
+  });
 })
