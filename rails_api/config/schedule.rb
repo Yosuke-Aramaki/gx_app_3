d56f23ce86a4e40e28cc5397dce1e910a07c155f
Yosuke-Aramaki <gnx.vw903@gmail.com>
2020-08-07 13:19:11 +0900

    set reminder cron #10

--- rails_api/config/schedule.rb
+++ rails_api/config/schedule.rb
@@ -7,25 +7,28 @@
 # wheneverにrailsを起動する必要があるためRails.rootを使用
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 
-
+# 環境変数をうまい感じにやってくれる
 ENV.each { |k, v| env(k, v) }
 
-set :output, error: 'log/crontab_error.log', standard: 'log/crontab.log' # ログを書き出すようにしておくとデバッグが楽
+# ログを書き出すようファイル
+set :output, error: 'log/crontab_error.log', standard: 'log/crontab.log'
 set :environment, :development
 
-# cronのログの場所
-# set :output, "log/cron.log" #,{ :error => 'log/whenever.log', :standard => 'log/cron.log' }
+# # cronを実行する環境変数 Dockerを使用しない場合
+# rails_env = ENV['RAILS_ENV'] || :development
+# # cronを実行する環境変数をセット
+# ENV['RAILS_ENV'] ||= 'development'
+# set :environment, ENV['RAILS_ENV']
 
-# every 1.hours, at: 40 do
-#   runner 'User.create(name: "cc", email: "cc@gmail.com", password: "cc", password_confirmation: "cc")'
-# end
+# set :job_template, "/bin/zsh -l -c ':job'"
+# job_type :rake,    "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
+# job_type :runner,  "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && bundle exec rails runner -e :environment ':task' :output"
 
 
 # every 1.hours, at: 46 do
 every 2.minutes do
-  # runner 'User.create(name: "dd", email: "dd@gmail.com", password: "cc", password_confirmation: "cc")'
-  # runner "Test.yakisoba", :environment => :development
   rake 'remind_task:check_remind_time'
+  # runner "Test.yakisoba", :environment => :development # runnnerの例
 end
 
 
