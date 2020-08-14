dbffa2577dbb1986d2f7818f0d072430520bb61d
Yosuke-Aramaki <gnx.vw903@gmail.com>
2020-08-14 09:27:41 +0900

    change og image url type to text from string #12

--- rails_api/db/migrate/20200721084511_create_articles.rb
+++ rails_api/db/migrate/20200721084511_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
     create_table :articles do |t|
       t.string :title
       t.string :article_url
-      t.string :og_image_url
+      t.text :og_image_url
       t.text :article_note
       t.boolean :is_read, default: false, null: false
       t.references :user, foreign_key: true
