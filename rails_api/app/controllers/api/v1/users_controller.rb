e2ad8cc560c13eb30e227ae4edb8c1849fa72560
Yosuke-Aramaki <gnx.vw903@gmail.com>
2021-03-26 14:43:44 +0900

    feature/pass_reset_api

--- rails_api/app/controllers/api/v1/users_controller.rb
+++ rails_api/app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :authenticate, only: [:create]
-  
+
   def index
-   
+
   end
 
   # ユーザー登録
@@ -14,6 +14,22 @@ class Api::V1::UsersController < ApplicationController
       render json: { token: token }
     else
       render json: { messages: @user.errors }, status: :unauthorized
+    end
+  end
+
+  # パスワード変更
+  def update
+    authenticate_or_request_with_http_token do |token, options|
+      user_token = Redis.current.get(token)
+      @user = User.find_by(id: user_token)
+      @user != nil ? true : false
+      if @user.update(password: params[:user][:password], password_confirmation: params[:user][:password_confirmation])
+        token = SecureRandom.hex(18)
+        Redis.current.set(token, @user.id)
+        render json: { token: token }
+      else
+        render json: { messages: @user.errors }, status: :unauthorized
+      end
     end
   end
 
